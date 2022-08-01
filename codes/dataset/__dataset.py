5d69a85fd52a0a6c0326d21395bb61f210be8c7b
conghaowong <conghao_wong@icloud.com>
2022-08-01 18:20:48 +0800

    move process codes here

---
+++ codes/dataset/__dataset.py
@@ -0,0 +1,121 @@
+"""
+@Author: Conghao Wong
+@Date: 2022-08-01 15:34:30
+@LastEditors: Conghao Wong
+@LastEditTime: 2022-08-01 18:18:46
+@Description: file content
+@Github: https://github.com/cocoon2wong
+@Copyright 2022 Conghao Wong, All Rights Reserved.
+"""
+
+import os
+
+from codes.utils import dir_check, write_plist
+
+from .__videoClip import VideoClip
+
+
+class Dataset():
+
+    VideoClipType = VideoClip
+
+    # Saving paths
+    BASE_DIR = './dataset_configs'
+    CONFIG_FILE = os.path.join(BASE_DIR, '{}', '{}.plist')
+
+    def __init__(self, name: str,
+                 type: str,
+                 scale: float,
+                 scale_vis: float,
+                 dimension: int,
+                 anntype: str) -> None:
+
+        self.__name = name
+        self.__type = type
+        self.__scale = scale
+        self.__scale_vis = scale_vis
+        self.__dimension = dimension
+        self.__anntype = anntype
+
+        self.clips: list[VideoClip] = []
+        self.CONFIG_FILE = self.CONFIG_FILE.format(self.name, '{}')
+
+        config_dir = os.path.dirname(self.CONFIG_FILE)
+        dir_check(config_dir)
+
+    def set_videoClip_type(self, new_type: type[VideoClip]):
+        self.VideoClipType = new_type
+
+    def add_clips(self, clips: list[str], force_update=False):
+        for clip in clips:
+            vc = self.VideoClipType(name=clip, dataset=self.name,
+                                    datasetInfo=self)
+            vc.force_update = force_update
+            self.clips.append(vc.get())
+
+    def save_clips_info(self):
+        for clip in self.clips:
+            clip.save_info()
+
+    def get_splits(self):
+        raise NotImplementedError(
+            'Please re-write this method when subclassing.')
+
+    def save_splits_info(self):
+        for split in self.get_splits():
+            dic = dict(train=split[0],
+                       test=split[1],
+                       val=split[2],
+                       dataset=self.name,
+                       scale=self.scale,
+                       dimension=self.dimension,
+                       scale_vis=self.scale_vis,
+                       anntype=self.anntype,
+                       type=self.type)
+
+            write_plist(dic, p := self.CONFIG_FILE.format(split[-1]))
+            print('Successfully saved at {}'.format(p))
+
+    @property
+    def name(self) -> str:
+        """
+        Name of the dataset.
+        """
+        return self.__name
+
+    @property
+    def type(self) -> str:
+        """
+        Annotation type of the dataset.
+        For example, `'pixel'` or `'meter'`.
+        """
+        return self.__type
+
+    @property
+    def scale(self) -> float:
+        """
+        Global data scaling scale.
+        """
+        return self.__scale
+
+    @property
+    def scale_vis(self) -> float:
+        """
+        Video scaling when saving visualized results.
+        """
+        return self.__scale_vis
+
+    @property
+    def dimension(self) -> int:
+        """
+        Dimension of annotations.
+        """
+        return self.__dimension
+
+    @property
+    def anntype(self) -> str:
+        """
+        Type of annotations.
+        For example, `'coordinate'` or `'boundingbox'`.
+        """
+        return self.__anntype
---
+++ codes/dataset/__videoClip.py
@@ -0,0 +1,132 @@
+"""
+@Author: Conghao Wong
+@Date: 2022-08-01 15:42:08
+@LastEditors: Conghao Wong
+@LastEditTime: 2022-08-01 18:20:09
+@Description: file content
+@Github: https://github.com/cocoon2wong
+@Copyright 2022 Conghao Wong, All Rights Reserved.
+"""
+
+import os
+
+from ..utils import dir_check, load_from_plist, write_plist
+
+
+class VideoClip():
+
+    # Dataset annotation files
+    PROCESSED_FILE = 'ann.csv'
+    SOURCE_FILE = None
+    TARGET_FILE = './dataset_processed/{}/{}/' + PROCESSED_FILE
+
+    # Saving paths
+    BASE_DIR = './dataset_configs'
+    CONFIG_FILE = os.path.join(BASE_DIR, '{}', 'subsets', '{}.plist')
+
+    saveKeys = ['name', 'annpath', 'order',
+                'paras', 'video_path',
+                         'matrix', 'dataset']
+
+    def __init__(self, name: str,
+                 dataset: str,
+                 annpath: str = None,
+                 order: tuple[int, int] = None,
+                 paras: tuple[int, int] = None,
+                 video_path: str = None,
+                 matrix: list[float] = None,
+                 datasetInfo=None,
+                 *args, **kwargs):
+
+        self.__name = name
+        self.__annpath = annpath
+        self.__order = order
+        self.__paras = paras
+        self.__video_path = video_path
+        self.__matrix = matrix
+        self.__dataset = dataset
+
+        self.datasetInfo = datasetInfo
+        self.force_update = False
+
+        self.TARGET_FILE = self.TARGET_FILE.format(self.dataset, self.name)
+        self.CONFIG_FILE = self.CONFIG_FILE.format(self.dataset, self.name)
+
+        # make dirs
+        dirs = [self.TARGET_FILE, self.CONFIG_FILE]
+        for d in dirs:
+            _dir = os.path.dirname(d)
+            dir_check(_dir)
+
+    @property
+    def dataset(self) -> str:
+        """
+        Name of the dataset.
+        """
+        return self.__dataset
+
+    @property
+    def name(self):
+        """
+        Name of the video clip.
+        """
+        return self.__name
+
+    @property
+    def annpath(self) -> str:
+        """
+        Path of the annotation file. 
+        """
+        return self.__annpath
+
+    @property
+    def order(self) -> list[int]:
+        """
+        X-Y order in the annotation file.
+        """
+        return self.__order
+
+    @property
+    def paras(self) -> tuple[int, int]:
+        """
+        [sample_step, frame_rate]
+        """
+        return self.__paras
+
+    @property
+    def video_path(self) -> str:
+        """
+        Path of the video file.
+        """
+        return self.__video_path
+
+    @property
+    def matrix(self) -> list[float]:
+        """
+        transfer weights from real scales to pixels.
+        """
+        return self.__matrix
+
+    def transfer_annotations(self):
+        raise NotImplementedError(
+            'Please re-write this method when subclassing.')
+
+    def get(self):
+        plist_path = self.CONFIG_FILE
+
+        if self.force_update or not os.path.exists(plist_path):
+            dic = self.transfer_annotations()
+        else:
+            dic = load_from_plist(plist_path)
+
+        self.__init__(**dic)
+        return self
+
+    def save_info(self):
+        """
+        Save dataset information into `plist` files.
+        """
+        values = [getattr(self, name) for name in self.saveKeys]
+        dic = dict(zip(self.saveKeys, values))
+        write_plist(dic, self.CONFIG_FILE)
+        print(f'Successfully saved at {self.CONFIG_FILE}')
